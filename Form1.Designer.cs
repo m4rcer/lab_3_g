62206525e74afec481a8423dd2b0277c846d9af7
m4rcer <m4rcer@gmail.com>
2022-11-29 20:47:34 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace lab_3_g
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.writeBtn = new System.Windows.Forms.Button();
+            this.showBtn = new System.Windows.Forms.Button();
+            this.clearBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(-2, -1);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(1645, 981);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // writeBtn
+            // 
+            this.writeBtn.Location = new System.Drawing.Point(116, 937);
+            this.writeBtn.Name = "writeBtn";
+            this.writeBtn.Size = new System.Drawing.Size(165, 43);
+            this.writeBtn.TabIndex = 1;
+            this.writeBtn.Text = "Записать в файл";
+            this.writeBtn.UseVisualStyleBackColor = true;
+            this.writeBtn.Click += new System.EventHandler(this.writeBtn_Click);
+            // 
+            // showBtn
+            // 
+            this.showBtn.Location = new System.Drawing.Point(398, 937);
+            this.showBtn.Name = "showBtn";
+            this.showBtn.Size = new System.Drawing.Size(165, 43);
+            this.showBtn.TabIndex = 1;
+            this.showBtn.Text = "Отобразить";
+            this.showBtn.UseVisualStyleBackColor = true;
+            this.showBtn.Click += new System.EventHandler(this.showBtn_Click);
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Location = new System.Drawing.Point(696, 937);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(165, 43);
+            this.clearBtn.TabIndex = 1;
+            this.clearBtn.Text = "Очистить";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1645, 1050);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.showBtn);
+            this.Controls.Add(this.writeBtn);
+            this.Controls.Add(this.pictureBox);
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Lab 3";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button writeBtn;
+        private System.Windows.Forms.Button showBtn;
+        private System.Windows.Forms.Button clearBtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_3_g
+{
+    public partial class Form1 : Form
+    {
+        public string[] strings = new string[14]
+        {
+            "Fist line",
+            "Second line",
+            "Third line",
+            "Fourth line",
+            "Fifth line",
+            "Sixth line",
+            "Seventh line",
+            "Eighth line",
+            "Ninth line",
+            "Tenth line",
+            "Eleven line",
+            "Twelve line",
+            "Thirteenth line",
+            "Forteenth line"
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void writeBtn_Click(object sender, EventArgs e)
+        {
+            StreamWriter fileStream = new StreamWriter("strings.txt", false);
+            foreach(string s in strings)
+            {
+                fileStream.WriteLine(s);
+            }
+            fileStream.Close();
+        }
+
+        private string[] readFromFile(string filename)
+        {
+            StreamReader reader = new StreamReader(filename);
+            List<string> strings = new List<string>();
+
+            while(reader.Peek() != -1)
+            {
+                strings.Add(reader.ReadLine());
+            }
+            reader.Close();
+
+            return strings.ToArray();
+        }
+
+        private void showBtn_Click(object sender, EventArgs e)
+        {
+            Graphics g = pictureBox.CreateGraphics();
+            string[] strings = readFromFile("strings.txt");
+
+            Font font1 = new Font("Imprint MT Shadow", 24, FontStyle.Regular);
+            Font font2 = new Font("Arial Black", 18, FontStyle.Bold);
+            Font font3 = new Font("Corbel", 66, FontStyle.Italic);
+
+            StringBuilder stringBuilder1 = new StringBuilder();
+            StringBuilder stringBuilder2 = new StringBuilder();
+            StringBuilder stringBuilder3 = new StringBuilder();
+
+            StringFormat stringFormat1 = new StringFormat();
+            stringFormat1.Alignment = StringAlignment.Near;
+            stringFormat1.LineAlignment = StringAlignment.Far;
+
+            StringFormat stringFormat2 = new StringFormat();
+            stringFormat2.Alignment = StringAlignment.Center;
+            stringFormat2.LineAlignment = StringAlignment.Far;
+
+            StringFormat stringFormat3 = new StringFormat();
+            stringFormat3.Alignment = StringAlignment.Near;
+            stringFormat3.FormatFlags = StringFormatFlags.DirectionVertical;
+            stringFormat3.LineAlignment = StringAlignment.Center;
+
+            for (int i = 0; i < 6; i++)
+            {
+                stringBuilder1.AppendLine(strings[i]);
+            }
+
+            for (int i = 6; i < 13; i++)
+            {
+                stringBuilder2.AppendLine(strings[i]);
+            }
+
+            for (int i = 13; i < 14; i++)
+            {
+                stringBuilder3.AppendLine(strings[i]);
+            }
+
+            string string1 = stringBuilder1.ToString();
+            string string2 = stringBuilder2.ToString();
+            string string3 = stringBuilder3.ToString();
+
+            g.DrawString(string1, font1, Brushes.Black, 500, 250, stringFormat1);
+            g.DrawString(string2, font2, Brushes.Red, 110, 250, stringFormat2);
+            g.DrawString(string3, font3, Brushes.Green, 900, 0, stringFormat3);
+
+            font1.Dispose();
+            font2.Dispose();
+            font3.Dispose();
+            stringFormat1.Dispose();
+            stringFormat2.Dispose();
+            stringFormat3.Dispose();
+            g.Dispose();
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            Graphics g = pictureBox.CreateGraphics();
+            g.Clear(Color.FromKnownColor(KnownColor.Control));
+            g.Dispose();
+        }
+    }
+}
